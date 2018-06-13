1a57435cbbf3bf8e06d9ac019d3d411a2edbefa9
jose barboza <jrbarboza08@gmail.com>
2018-06-13 20:01:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,42 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+ const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+let index = 0;
+
+function init() {
+  document.getElementById("myBody").addEventListener("keydown", konamiKode);
+  
+  function konamiKode(e){
+    const key = e.key;
+    
+    if (key===codes[index]){
+      index++;
+      
+      if (index === codes.length) {
+        alert("You win $1000,000!!");
+        index = 0;
+      } 
+   
+      
+    } else {
+        index = 0;
+    }
+  }
+ 
+} 
+  
+  
 }
